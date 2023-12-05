1bd916c079e86c823fb5d2cb3a379f9a16246190
Ksenia Morunova <ksenia.morunova@mhp.com>
2023-12-05 11:54:43 +0100

    Hello Lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,37 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
 
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+// Cleaning the sample code
+// import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+// import * as sns from 'aws-cdk-lib/aws-sns';
+// import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import { Construct } from 'constructs';
+
+// export class CdkWorkshopStack extends Stack {
+//   constructor(scope: Construct, id: string, props?: StackProps) {
+//     super(scope, id, props);
+
+//     const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+//       visibilityTimeout: Duration.seconds(300)
+//     });
+
+//     const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+
+//     topic.addSubscription(new subs.SqsSubscription(queue));
+//   }
+// }
+
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    
+    // Define AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X, // exec env
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" dir
+      handler: 'hello.handler' // file is "hello", func is "handler"
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
