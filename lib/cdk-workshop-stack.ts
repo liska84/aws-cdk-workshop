4e913c452d647d03f7c1d286baa911d52ca9bb88
Ksenia Morunova <ksenia.morunova@mhp.com>
2023-12-05 14:56:53 +0100

    Lambda + API Gateway

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -20,18 +20,42 @@
 //   }
 // }
 
+// Lambda example
+// import * as cdk from 'aws-cdk-lib';
+// import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+// export class CdkWorkshopStack extends cdk.Stack {
+//   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+    
+//     // Define AWS Lambda resource
+//     const hello = new lambda.Function(this, 'HelloHandler', {
+//       runtime: lambda.Runtime.NODEJS_16_X, // exec env
+//       code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" dir
+//       handler: 'hello.handler' // file is "hello", func is "handler"
+//     });
+//   }
+// }
+
+//API Gateway + Lambda
 import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import* as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
-    // Define AWS Lambda resource
+
+    // Lambda 
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X, // exec env
-      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" dir
+      runtime:lambda.Runtime.NODEJS_16_X, // exec env
+      code:lambda.Code.fromAsset('lambda'), // code loader from "lambda" dir
       handler: 'hello.handler' // file is "hello", func is "handler"
+    });
+
+    // API Gateway REST API resource backed by "hello" function
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
   }
 }
