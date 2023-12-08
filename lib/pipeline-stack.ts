fe2d39082f7968705207a3143c75762b9980c3b3
Ksenia Morunova <ksenia.morunova@mhp.com>
2023-12-08 10:47:19 +0100

    Deploy workshop pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
         
         // CodeCommit Repo
-        new codecommit.Repository(this, 'WorkshopRepo', {
+        const repo = new codecommit.Repository(this, 'WorkshopRepo', {
             repositoryName: 'workshop',
         });
 
-        // Pipeline code
+        // Basic pipeline declaration, sets the initial structure of the pipeline
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'WorkshopPipeline', 
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.codeCommit(repo, 'main'),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth', // cdk synth ?
+                ]
+            })
+        });
     }
 }
